9bbe20fcc47bc3766af5f1e9c120fd3659763dcc
Кирилл <kiboto30@gmail.com>
2025-12-23 20:58:50 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -18,5 +18,10 @@ def health():
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     app.run(host="0.0.0.0", port=5000)
+import os
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 
